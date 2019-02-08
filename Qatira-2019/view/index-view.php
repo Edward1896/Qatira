a769850350e36c1eb145616ec166376d8512117e
GaboGabriel <44986617+GaboGabriel@users.noreply.github.com>
2019-02-07 23:59:27 -0500

    Add files via upload

---
+++ Qatira-2019/view/index-view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+	<meta name="keywords" content="Anuncios Gratis">
+	<meta name="description" content="Qatira es una plataforma..............">
+	<meta name="author" content="Untoke">
+  <title>Inicio - Qatira</title>
+
+  <!--icons-->
+  <link rel="icon" type="image/png" href="#" />
+	<!-- cdn  fontawesome-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">			
+	<!-- cdn boostrap 4-->
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!--carousel-->
+  <link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/examples/carousel/carousel.css">
+  <!--Estilos-->
+  <link rel="stylesheet" href="view/default/assets/css/style.css">
+
+
+</head>
+<body>
+	
+
+ <!-- Navigation -->
+  <?php  include 'view/default/sections/navbar.html'; ?>
+
+  <!--header-->
+  <?php  include 'view/default/sections/header.html'; ?>
+
+   <!-- Page Content -->
+  <?php  include 'view/default/sections/main.html'; ?>
+
+   <!-- Footer -->
+  <?php include 'view/default/sections/footer.html'; ?>
+
+  <!-- Bootstrap core JavaScript -->
+    <script src="view/default/assets/jquery/jquery.min.js"></script>
+  <!--carousel-->
+    <script src="view/default/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
